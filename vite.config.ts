e5b2ee089b61025040cd50311295c5c0d7a9d66f
Decency Okobia <learncodingdee@yahoo.com>
2025-04-09 12:20:36 +0100

    added build file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'  // Make sure the build output directory is 'build'
+  }
 })
+// vite.config.js
+
+  
